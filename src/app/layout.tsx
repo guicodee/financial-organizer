6aaf96e26d09ca1b432d8b78d6889e7615c72c14
guicodee <guilhermedfgo12@gmail.com>
2024-10-02 14:43:13 -0300

    fix(lint): fixes linting errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,23 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google/';
-import "./globals.css";
+import './globals.css';
 
-const roboto = Roboto({ subsets: ["latin"], weight: "400" })
+const roboto = Roboto({ subsets: ['latin'], weight: '400' });
 
 export const metadata: Metadata = {
-  title: "Organizador financeiro",
-  description: "A ferramenta ideal para quem busca organizar suas finanças de forma prática e eficiente.",
+	title: 'Organizador financeiro',
+	description:
+		'A ferramenta ideal para quem busca organizar suas finanças de forma prática e eficiente.',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR">
-      <body
-        className={`${roboto.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="pt-BR">
+			<body className={`${roboto.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
