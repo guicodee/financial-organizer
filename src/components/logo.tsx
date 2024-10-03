d12fea2dca5c93da1d71bb806e14e5ce7d988267
guicodee <guilhermedfgo12@gmail.com>
2024-10-03 14:00:17 -0300

    feat: add toggle theme button

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,11 @@
-import { PiggyBank } from 'lucide-react';
+import { Eclipse } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Logo() {
 	return (
 		<Link href="/" className="flex items-center gap-2">
-			<PiggyBank className="h-11 w-11 stroke stroke-amber-500 stroke-[1.5]" />
-			<p className="font-bold bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl tracking-tighter text-transparent">
+			<Eclipse className="md:h-8 md:w-8 max-md:hidden stroke stroke-yellow-400 stroke-[1.5]" />
+			<p className="font-bold md:text-xl max-md:text-sm tracking-wider">
 				Financial Organizer
 			</p>
 		</Link>
