434b38a02867ce95bfe07a9825e7e7dfbb98c77f
guicodee <guilhermedfgo12@gmail.com>
2024-10-03 15:33:11 -0300

    feat: add tanstack react query in root providers

--- src/components/providers/root-providers.tsx
+++ src/components/providers/root-providers.tsx
@@ -1,15 +1,22 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'next-themes';
 import { ReactNode } from 'react';
 
 export default function RootProviders({ children }: { children: ReactNode }) {
+	const queryClient = new QueryClient();
+
 	return (
-		<ThemeProvider
-			attribute="class"
-			defaultTheme="system"
-			enableSystem
-			disableTransitionOnChange
-		>
-			{children}
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				enableSystem
+				disableTransitionOnChange
+			>
+				{children}
+			</ThemeProvider>
+		</QueryClientProvider>
 	);
 }
