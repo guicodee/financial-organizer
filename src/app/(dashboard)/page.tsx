89a9fead53a140d5e9e5d8377731b5110f7636e5
guicodee <guilhermedfgo12@gmail.com>
2024-10-05 14:30:10 -0300

    feat: add income and expense button in dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,55 @@
-export default function Page() {
-	return <div>Hello world</div>;
+import { Button } from '@/components/ui/button';
+import prisma from '@/lib/prisma';
+import { currentUser } from '@clerk/nextjs/server';
+import { CreditCard, DollarSign } from 'lucide-react';
+import { redirect } from 'next/navigation';
+import DialogTransaction from './components/dialog-transaction';
+
+export default async function Page() {
+	const user = await currentUser();
+	if (!user) redirect('/sign-in');
+
+	const userSettings = await prisma.userSettings.findUnique({
+		where: {
+			userId: user.id,
+		},
+	});
+	if (!userSettings) redirect('/sign-in');
+
+	return (
+		<div className="h-full bg-background">
+			<div className="border-b bg-card">
+				<div className="container flex flex-wrap items-center justify-between gap-6 py-8 mx-auto px-8">
+					<p className="text-2xl font-bold">Olá, {user.firstName}! 👋</p>
+
+					<div className="flex items-center gap-3">
+						<DialogTransaction
+							trigger={
+								<Button
+									variant={'outline'}
+									className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-800 flex items-center gap-1"
+								>
+									Ganhos
+									<DollarSign size={14} color="#00FF00" />
+								</Button>
+							}
+							type="income"
+						/>
+						<DialogTransaction
+							trigger={
+								<Button
+									variant={'outline'}
+									className="border-rose-500 bg-rose-950 text-white hover:bg-rose-800 flex items-center gap-1"
+								>
+									Gastos
+									<CreditCard size={14} color="#FF4040" />
+								</Button>
+							}
+							type="expense"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
