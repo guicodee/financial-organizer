a82b00b324ad67abd6c36693337def73e2e9d3ea
guicodee <guilhermedfgo12@gmail.com>
2024-10-07 18:02:45 -0300

    feat: add helpers to date in transactions

---
+++ src/lib/helpers.ts
@@ -0,0 +1,13 @@
+export default function DateToUTCDate(date: Date) {
+	return new Date(
+		Date.UTC(
+			date.getFullYear(),
+			date.getMonth(),
+			date.getDay(),
+			date.getHours(),
+			date.getMinutes(),
+			date.getSeconds(),
+			date.getMilliseconds()
+		)
+	);
+}
